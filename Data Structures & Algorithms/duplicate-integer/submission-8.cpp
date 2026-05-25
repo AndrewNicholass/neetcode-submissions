3cf0d4563557143df5d9ac29215dac089e1c3107
Andrew Nicholas <119880958+AndrewNicholass@users.noreply.github.com>
2026-05-25 22:42:50 +0700

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+        unordered_map<int, int> hash;
+
+        for(int i = 0; i < nums.size(); i++){
+            hash[nums[i]]++;
+        }
+
+        for(auto num : hash){
+            if(num.second > 1){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+};
